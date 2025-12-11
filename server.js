eb3caddc6ae861399275b778a673c3770bd54248
saur725put1234 <sourabh@digirockett.com>
2025-12-11 18:16:20 +0530

    cors testing

--- server.js
+++ server.js
@@ -20,14 +20,16 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true,
-    allowedHeaders: ["Content-Type", "Accept", "Authorization"],
-    methods: ["GET", "POST", "PUT","DELETE"],
-  })
-);
+// app.use(
+//   cors({
+//     origin: process.env.FRONTEND_URL,
+//     credentials: true,
+//     allowedHeaders: ["Content-Type", "Accept", "Authorization"],
+//     methods: ["GET", "POST", "PUT","DELETE"],
+//   })
+// );
+app.use(cors());
+
 app.use(cookieParser());
 
 app.use(express.json({ limit: "10mb" }));
