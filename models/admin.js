70810d69e8e04cc8c721db6a936cf740d9e7bfd5
saur725put1234 <sourabh@digirockett.com>
2025-12-18 18:49:47 +0530

    user model and controllers created

---
+++ models/admin.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const AdminSchema = mongoose.Schema(
+    {
+        email: String,
+        password: String
+    }
+)
+
+const AdminModel = mongoose.model('admin', AdminSchema);
+export default AdminModel;
